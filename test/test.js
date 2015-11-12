1f5bdcd95df5170573985a4997822304e4f5b3f0
Guillaume Prin <guil.prin@gmail.com>
2015-11-12 13:28:27 +0100

    correctif relatif a histoire 1 : un ! trainait sans raison

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ PalettoTestCase.prototype.testStory1 = function () {
     var i, j;
     for(i = 0 ; i < 5 ; i++) {
         for(j = 0 ; j < 5 ; j++) {
-            !assertEquals(board.isColoredAdjacent(i,j), false);
+            assertEquals(board.isColoredAdjacent(i,j), false);
         }
     }
 };
