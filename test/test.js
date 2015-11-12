74aacf33e13c30994fdc6f032d1a8a613c1ec2dc
Guillaume Prin <guil.prin@gmail.com>
2015-11-12 13:54:43 +0100

    test 3

--- test/test.js
+++ test/test.js
@@ -16,3 +16,9 @@ PalettoTestCase.prototype.testStory2 = function () {
     board.pickColor('y');
     assertEquals(board.getPickedColor(), 'y');
 };
+
+PalettoTestCase.prototype.testStory3 = function () {
+    board.pickMarble("A6");
+    assertEquals(board.getNbMarbles(), 35);
+    assertEquals(board.getNbColoredMarbles('y'), 1);
+};
